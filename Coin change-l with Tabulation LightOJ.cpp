a4e068593a75dc39069297667a5948bb5b982d25
MD. Mehrabul Islam Zeshan <102683822+MehrabZishaan@users.noreply.github.com>
2023-07-12 21:46:11 +0600

    DP problem solved in both methods.

---
+++ Coin change-l with Tabulation LightOJ.cpp
@@ -0,0 +1,65 @@
+/// https://lightoj.com/problem/coin-change-i
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 60;
+const int M = 1001;
+const int MOD = 100000007;
+int dp[N][M];
+int coin[N], limit[N];
+
+int main()
+{
+    int t, tst = 0;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, K;
+        cin>> n >> K;
+        for(int i=1; i<=n; i++)
+        {
+            cin >> coin[i];
+        }
+        for(int i=1; i<=n; i++)
+        {
+            cin >> limit[i];
+        }
+         for(int i=0; i<=n; i++)
+        {
+            for(int target =0; target<=K; target++)
+            {
+                dp[i][target] = 0;
+            }
+        }
+
+        /// 1. Handle base case
+        dp[0][0] = 1;
+
+        /// 2. Loop over the states
+        for(int i =1; i<=n; i++)
+        {
+            for(int target = 0; target <= K; target++)
+            {
+                /// 2.1 calculate answer from smaller sub problems
+                dp[i][target] = dp[i-1][target];
+
+                for(int j=1; j<= limit[i]; j++)
+                {
+                    if(target < j*coin[i])
+                    {
+                        break;
+                    }
+                    int ret = dp[i-1][target - j*coin[i]];
+                    dp[i][target] = (dp[i][target] + ret) % MOD;
+                }
+            }
+        }
+
+        cout<<"Case "<< ++tst <<": "<< dp[n][K] <<"\n";
+
+    }
+
+    return 0;
+}
